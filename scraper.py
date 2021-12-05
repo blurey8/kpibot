10a88c1e4022799467d0a9e22df23890ffce3788
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2021-12-05 14:56:28 +0700

    Add verify=false on request get

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ PARAMETER_KEY = "detail3"
 
 def get_index_complains_anchors():
     logger.info("Access KPI page...")
-    index_page = requests.get(URL)
+    index_page = requests.get(URL, verify=False)
     index_soup = BeautifulSoup(index_page.content, "html.parser")
     index_box = index_soup.find(text=TITLE).parent.parent.parent
     complains = index_box.find_all("a")
@@ -23,7 +23,7 @@ def get_index_complains_anchors():
 def get_complain_details(complain_anchor):
     detail_url = complain_anchor["href"]
     logger.info("Get complain detail...")
-    detail_page = requests.get(detail_url)
+    detail_page = requests.get(detail_url, verify=False)
     detail_soup = BeautifulSoup(detail_page.content, "html.parser")
     detail_box = detail_soup.find(text=TITLE).parent.parent.parent
 
